cbb47c8d02058aec7d778753e30e13c0c8296b41
Niklas Lindblad <niklas@lindblad.info>
2016-12-04 18:38:57 +0000

    Fix comments for Matrix

--- src/main/java/info/lindblad/radio/solver/model/Matrix.java
+++ src/main/java/info/lindblad/radio/solver/model/Matrix.java
@@ -24,18 +24,45 @@ public class Matrix {
 
     private int[][] matrix;
 
-    public Matrix(List<TransmitterTower> transmitterTowers, List<ReceiverTower> receiverTowers) {
+    /**
+     * Construct a matrix representation of a given transmitter/receiver configuration
+     *
+     * @param transmitterTowers List of transmitter towers
+     * @param receiverTowersWithoutCoverage List of receiver towers that are without signal coverage
+     */
+    public Matrix(List<TransmitterTower> transmitterTowers, List<ReceiverTower> receiverTowersWithoutCoverage) {
         newTransmitterTowerPowerLevels = new HashMap<>();
         this.transmitterTowers = transmitterTowers;
-        this.receiverTowers = receiverTowers;
+        this.receiverTowers = receiverTowersWithoutCoverage;
         this.totalPowerIncrease = 0;
         matrix = constructInitialMatrix();
     }
 
+    /**
+     * Empty constructor that is only used internally for doing a deep copy of the matrix.
+     */
     private Matrix() {
 
     }
 
+    /**
+     * Construct the initial matrix using the internal list of transmitter and receiver towers.
+     *
+     * Each row in the matrix corresponds to a transmitter tower and each column corresponds to a receiver tower.
+     *
+     * The value in each cell represents the required increase in power for the transmitter tower to give signal
+     * coverage to the receiver tower.
+     *
+     * For the known provided test case, the matrix is:
+     *
+     *      R2
+     *  T3  5
+     *  T2  5
+     *  T1  5
+     *  T4  2
+     *
+     * @return The initial matrix as a 2D array of integers
+     */
     private int[][] constructInitialMatrix() {
         nbrOfRows = transmitterTowers.size();
         nbrOfColumns = receiverTowers.size();
@@ -53,18 +80,46 @@ public class Matrix {
         return matrix;
     }
 
+    /**
+     * Get the number of matrix rows
+     *
+     * @return The number of matrix rows
+     */
     public int getNbrOfRows() {
         return this.nbrOfRows;
     }
 
+    /**
+     * Get the number of matrix columns
+     *
+     * @return The number of matrix columns
+     */
     public int getNbrOfColumns() {
         return this.nbrOfColumns;
     }
 
+    /**
+     * Get the value of a cell in the matrix
+     *
+     * @param column The cell column
+     * @param row The cell row
+     * @return The value of the cell
+     */
     public int getValue(int column, int row) {
         return matrix[row][column];
     }
 
+
+    /**
+     * Choose a cell as a solution to the optimisation for a particular transmitter/receiver pairing
+     *
+     * Returns the value of the cell (the increase in power for the transmission tower) and updates
+     * the matrix row to reflect the change.
+     *
+     * @param column The cell column
+     * @param row The cell row
+     * @return The value of the cell
+     */
     public int choose(int column, int row) {
         int chosenValue = matrix[row][column];
         totalPowerIncrease += chosenValue;
@@ -76,6 +131,12 @@ public class Matrix {
         return chosenValue;
     }
 
+    /**
+     * Get a list of row indices indicating the minimal value seen in the column
+     *
+     * @param column The column
+     * @return A list of the row indices containing the minimal value seen in the column
+     */
     public List<Integer> getMinimumRows(int column) {
         SimplePriorityQueue<Integer> lowestValueIndices = new SimplePriorityQueue<>();
         for (int row = 0; row < matrix.length; row++) {
@@ -84,6 +145,11 @@ public class Matrix {
         return new ArrayList<>(lowestValueIndices.pollSmallest().getValue());
     }
 
+    /**
+     * Get a map of transmitter towers and their required new power level
+     *
+     * @return A map of transmitter towers and their required new power level
+     */
     public Map<TransmitterTower, Integer> getNewTransmitterTowerPowerLevels() {
         Map<TransmitterTower, Integer> newTransmitterTowerPowerLevels= new HashMap<>();
         for (Map.Entry<TransmitterTower, AtomicInteger> entry : this.newTransmitterTowerPowerLevels.entrySet()) {
@@ -92,10 +158,20 @@ public class Matrix {
         return newTransmitterTowerPowerLevels;
     }
 
+    /**
+     * Get the total power increase so far for this solution matrix
+     *
+     * @return The total power increase so far for this solution matrix
+     */
     public Integer getTotalPowerIncrease() {
         return totalPowerIncrease;
     }
 
+    /**
+     * Get a string representation of the matrix
+     *
+     * @return String representation of the matrix
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -114,8 +190,15 @@ public class Matrix {
         return sb.toString();
     }
 
+    /**
+     * Create a deep copy of the matrix that can withstand internal state being mutated.
+     *
+     * This is necessary in order to facility branching out to solve cases where there are
+     * multiple options for the next step.
+     *
+     * @return A deep copy of the matrix
+     */
     public Matrix copy() {
-        // TODO(nlindblad) Clean this up
         int [][] copiedMatrix = new int[matrix.length][];
         for (int i = 0; i < matrix.length; i++)
         {
